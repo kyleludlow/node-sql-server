387f968f24db22f23f3915cdb27b043e19cd7806
Kyle <kyleludlow@gmail.com>
2017-01-21 21:36:56 -0800

    adds todos with key generator, get all functionality and query filtering for completed and description

--- server.js
+++ server.js
@@ -1,31 +1,50 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var _ = require('underscore');
+var express = require('express')
+var bodyParser = require('body-parser')
+var _ = require('underscore')
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+var app = express()
+var PORT = process.env.PORT || 3000
 
-app.use(bodyParser.json());
+var todos = []
+var todoNextId = 1
 
-app.get('/', function(req, res) {
-  res.send('Root.');
-});
+app.use(bodyParser.json())
 
-app.get('/todos', function(req, res) {
-});
+app.get('/', function (req, res) {
+  res.send('Root.')
+})
 
-app.get('/todos/:id', function(req, res) {
-});
+app.get('/todos', function (req, res) {
+  var queryParams = req.query
+  var filteredTodos = todos
 
-app.post('/todos', function(req, res) {
-});
+  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {completed: true})
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false})
+  }
 
-app.delete('/todos/:id', function(req, res) {
-});
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(filteredTodos, function (todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1
+    })
+  }
 
-app.put('/todos/:id', function(req, res) {
-});
+  res.json(filteredTodos)
+})
 
-app.listen(PORT, function() {
+app.get('/todos/:id', function (req, res) {
+})
+
+app.post('/todos', function (req, res) {
+})
+
+app.delete('/todos/:id', function (req, res) {
+})
+
+app.put('/todos/:id', function (req, res) {
+})
+
+app.listen(PORT, function () {
   console.log(`Server started on PORT: ${PORT}!`)
-});
+})
