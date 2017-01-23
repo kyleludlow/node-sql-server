15d9a4f4c79e9cd933d40a7bea8dcf47a81403a4
Kyle <kyleludlow@gmail.com>
2017-01-22 22:40:33 -0800

    adds sequelize, Postgres, SQlite, db config for sequelize to use sqlite for development and Postgres for production.  Adds new todo table, and todo model, runs db on server start

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var _ = require('underscore')
+var db = require('./db.js')
+
 
 var app = express()
 var PORT = process.env.PORT || 3000
@@ -99,6 +101,8 @@ app.put('/todos/:id', function (req, res) {
   res.json(matchedTodo)
 })
 
-app.listen(PORT, function () {
-  console.log(`Server started on PORT: ${PORT}!`)
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(`Server started on PORT: ${PORT}!`)
+  })
 })
